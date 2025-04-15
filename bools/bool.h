d84e8248cd0206a436481831be7cd49feea8239d
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-16 01:43:19 +0200

    Ajout de la gestion des mots-clés bool, true et false pour les versions C standard récentes.

--- bools/bool.h
+++ bools/bool.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
+/* bool, true and false are keywords.  */
+#else
+
 typedef unsigned char bool;
 
 #define true 1
 #define false 0
+
+#endif
