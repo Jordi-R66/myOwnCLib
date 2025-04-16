92768ad212608ba69d266760053f599d18d4226b
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-16 14:13:14 +0200

    Correction du type de retour de la fonction dotProduct pour utiliser value_t au lieu de double.

--- vectors/vectors.h
+++ vectors/vectors.h
@@ -13,4 +13,4 @@ void setCoord(Vector* vector, size_t coordNumber, value_t value);
 void setVector(Vector* vector, value_t* colBuffer);
 
 Vector crossProduct(Vector* vectorA, Vector* vectorB);
-double dotProduct(Vector* vectorA, Vector* vectorB);
+value_t dotProduct(Vector* vectorA, Vector* vectorB);
