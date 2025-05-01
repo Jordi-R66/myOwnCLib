c5d23aa408327b463a58ab5cb08fad34dc4f17db
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-01 19:36:35 +0200

    Révision de la fonction GET_BIT pour corriger la récupération d'un bit spécifique dans un mot.

--- bits/bits.c
+++ bits/bits.c
@@ -1,8 +1,9 @@
 #include "bits.h"
 
 uint8_t GET_BIT(uint64_t WORD, uint8_t BIT, uint8_t BIT_COUNT) {
-	WORD <<= (BIT_COUNT - BIT);
-	WORD >>= (BIT_COUNT - 1);
+	uint64_t CTRL_VAL = 1 << (BIT - 1);
 
-	return (uint8_t)WORD;
+	WORD &= CTRL_VAL;
+
+	return (uint8_t)(WORD == CTRL_VAL);
 }
