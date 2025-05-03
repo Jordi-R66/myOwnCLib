65d9bdd13b67599824bf889b943798fc30103658
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-03 17:13:30 +0200

    Ajout du fichier types.h pour définir des types de données basés sur la détection de la cible

--- targetDetection/macros.h
+++ targetDetection/macros.h
@@ -2,12 +2,12 @@
 
 #ifndef OVERRIDE_DETECTION
 
-#ifdef __x86_64__
-	#define __64_BITS__
-#elif defined(__i386__) || defined(__i386)
-	#define __32_BITS__
-#else
-	#error "Unknown target"
-#endif
+	#ifdef __x86_64__
+		#define __64_BITS__
+	#elif defined(__i386__) || defined(__i386)
+		#define __32_BITS__
+	#else
+		#error "Unknown target"
+	#endif
 
 #endif
