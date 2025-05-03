a70d92647f2ab5768c516522c6d14f9ac1dc912b
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-03 17:56:41 +0200

    Correction des inclusions dans localCommon.h pour assurer la cohérence des fichiers d'en-tête.

--- matrices/localCommon.h
+++ matrices/localCommon.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include "../targetDetection/macros.h"
+#include "../common.h"
 
 #ifdef __64_BITS__
 #define VALUE_TYPE double
 #elif defined(__32_BITS__)
 #define VALUE_TYPE float
 #endif
-
-#include "../common.h"
 
 typedef VALUE_TYPE value_t;
