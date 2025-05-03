3841072116f1ff70ed40c3f9ff47a17dfb67bae8
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-03 17:17:19 +0200

    Modification des types uint64 et int64 pour utiliser unsigned long long et signed long long

--- targetDetection/types.h
+++ targetDetection/types.h
@@ -6,10 +6,8 @@ typedef signed char int8;
 typedef unsigned short uint16;
 typedef signed short int16;
 
-#ifdef __64_BITS__
 typedef unsigned int uint32;
 typedef signed int int32;
 
-typedef unsigned long uint64;
-typedef signed long int64;
-#endif
+typedef unsigned long long uint64;
+typedef signed long long int64;
