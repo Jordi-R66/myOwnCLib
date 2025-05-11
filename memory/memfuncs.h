d1801067a9ff5c37b0d2d75c46935415f0f54ff7
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-11 22:33:14 +0200

    Ajout de la définition de l'énumération Comparison et de la fonction compareMemory pour comparer les blocs de mémoire.

--- memory/memfuncs.h
+++ memory/memfuncs.h
@@ -3,6 +3,10 @@
 #include "../bools/bool.h"
 #include "../targetDetection/types.h"
 
+typedef enum Comparison {
+	LOWER, EQUALS, GREATER
+} Comparison;
+
 /**
  * @brief Compares the n first bytes of A with the n first bytes of B
  * 
@@ -13,3 +17,13 @@
  * @return false The values are different
  */
 bool equalMemory(void* A, void* B, SizeT n);
+
+/**
+ * @brief Compares the n first bytes of A with the n first bytes of B
+ * 
+ * @param A Pointer to block A
+ * @param B Pointer to block B
+ * @param n Number of bytes to compare
+ * @return Whether A > B, A = B, A < B
+ */
+Comparison compareMemory(void* A, void* B, SizeT n);
