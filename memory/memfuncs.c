89cfb65aa43f546e43b5557d6c6a951b135620e9
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-11 22:51:59 +0200

    Ajout de la fonction compareMemory pour comparer les blocs de mémoire avec gestion de l'endianness.

--- memory/memfuncs.c
+++ memory/memfuncs.c
@@ -20,3 +20,33 @@ bool equalMemory(void* A, void* B, SizeT n) {
 
 	return equals;
 }
+
+Comparison compareMemory(void* A, void* B, SizeT n) {
+	if ((A == NULL) || (B == NULL)) {
+		exit(EXIT_FAILURE);
+	}
+
+	if (n == 0) {
+		return EQUALS;
+	}
+
+	Endianness_t endian = getEndian();
+
+	uint8* memA = (uint8*)A;
+	uint8* memB = (uint8*)B;
+
+	bool equals = true;
+
+	SizeT j = 0;
+
+	for (SizeT i = 0; (i < n) && equals; i++) {
+		j = endian == Little ? n - i - 1 : i;
+		equals &= memA[j] == memB[j];
+
+		if (!equals) {
+			return memA[j] > memB[j] ? GREATER : LESS;
+		}
+	}
+
+	return EQUALS;
+}
