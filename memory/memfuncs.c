723c062bdd88e99e5d489260f34d9047da42ed79
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-11 22:52:46 +0200

    Refactor la fonction equalMemory pour utiliser compareMemory, simplifiant ainsi le code.

--- memory/memfuncs.c
+++ memory/memfuncs.c
@@ -4,21 +4,7 @@
 #include "memfuncs.h"
 
 bool equalMemory(void* A, void* B, SizeT n) {
-
-	if ((A == NULL) || (B == NULL)) {
-		exit(EXIT_FAILURE);
-	}
-
-	uint8* memA = (uint8*)A;
-	uint8* memB = (uint8*)B;
-
-	bool equals = true;
-
-	for (SizeT i = 0; (i < n) && (equals); i++) {
-		equals &= memA[i] == memB[i];
-	}
-
-	return equals;
+	return (bool)(compareMemory(A, B, n) == EQUALS);
 }
 
 Comparison compareMemory(void* A, void* B, SizeT n) {
