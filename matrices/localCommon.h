cb3078c112e7ee9d9366fa5de04afb9e8bc2c35e
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-21 12:12:38 +0200

    Ajout d'une vérification pour éviter la redéfinition de VALUE_TYPE dans localCommon.h

--- matrices/localCommon.h
+++ matrices/localCommon.h
@@ -2,10 +2,12 @@
 
 #include "../common.h"
 
-#ifdef __64_BITS__
-#define VALUE_TYPE double
-#elif defined(__32_BITS__)
-#define VALUE_TYPE float
+#ifndef VALUE_TYPE
+	#ifdef __64_BITS__
+		#define VALUE_TYPE double
+	#elif defined(__32_BITS__)
+		#define VALUE_TYPE float
+	#endif
 #endif
 
 typedef VALUE_TYPE value_t;
