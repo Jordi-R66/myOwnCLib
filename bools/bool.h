37bce2433ac3f9bb880b9b41733b29e3caed1558
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-22 09:51:20 +0200

    Ajout de directives de préprocesseur pour inclure conditionnellement les définitions de booléens dans bool.h

--- bools/bool.h
+++ bools/bool.h
@@ -1,14 +1,16 @@
 #pragma once
 
-#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
-/* bool, true and false are keywords.  */
-#else
+#ifdef INCLUDE_BOOLS
+	#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
+	/* bool, true and false are keywords.  */
+	#else
 
-#define BOOL_BITS 8
+	#define BOOL_BITS 8
 
-typedef unsigned char bool;
+	typedef unsigned char bool;
 
-#define true 1
-#define false 0
+	#define true 1
+	#define false 0
 
+	#endif
 #endif
