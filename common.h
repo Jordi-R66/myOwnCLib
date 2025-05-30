d09078ed11e5a4b5fdf4e926af9536843f011674
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-05-30 12:35:39 +0200

    Suppression des directives de préprocesseur inutilisées dans common.h pour simplifier le code et améliorer la lisibilité

--- common.h
+++ common.h
@@ -1,21 +1,10 @@
 #pragma once
 
-#ifdef INCLUDE_COMMON
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef INCLUDE_BOOLS
-#define INCLUDE_BOOLS
-#endif
-
-#ifndef INCLUDE_TARGET_SPECIFIC
-#define INCLUDE_TARGET_SPECIFIC
-#endif
-
 #include "bools/bool.h"
 
-#include "targetSpecific/types.h"7
-#endif
+#include "targetSpecific/types.h"
