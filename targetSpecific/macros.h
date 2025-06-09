913c0ec3fd689aadd9e61b67c20a11dafbb81880
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-06-09 09:45:30 +0200

    Ajout de la détection d'architecture et de système dans target.h pour une meilleure gestion des plateformes.

--- targetSpecific/macros.h
+++ targetSpecific/macros.h
@@ -1,39 +1,6 @@
 #pragma once
 
-#ifndef OVERRIDE_DETECTION
-
-	#pragma region ArchDetection
-		#ifdef __x86_64__
-			#define __64_BITS__
-			#define INTEL_ARCH_FAMILY
-		#elif defined(__i386__) || defined(__i386) || defined(__i686__) || defined(__i686)
-			#define __32_BITS__
-			#define INTEL_ARCH_FAMILY
-		#elif defined(__ARM_ASM_SYNTAX_UNIFIED__)
-			#define ARM_ARCH_FAMILY
-			#if defined(__ARM_32BIT_STATE)
-				#define __32_BITS__
-			#elif defined(__ARM_64BIT_STATE)
-				#define __64_BITS__
-			#else
-				#error "Can't identify target ARM arch"
-			#endif
-		#else
-			#error "Unknown target architecture"
-		#endif
-	#pragma endregion
-
-	#pragma region SysDetection
-		#ifdef __linux
-			#define LINUX
-		#elif defined(_WIN64) || defined(_WIN32)
-			#define WINDOWS
-		#else
-			#error "Unknown target OS"
-		#endif
-	#pragma endregion
-
-#endif
+#include "target.h"
 
 #ifdef NULL
 	#undef NULL
