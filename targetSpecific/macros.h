41409929a5ce6a0f171a66b846d43be258c4f26b
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-06-09 09:03:51 +0200

    Correction du message d'erreur pour une architecture cible inconnue dans le fichier macros.h

--- targetSpecific/macros.h
+++ targetSpecific/macros.h
@@ -5,8 +5,16 @@
 		#define __64_BITS__
 	#elif defined(__i386__) || defined(__i386)
 		#define __32_BITS__
+	#elif defined(__ARM_ASM_SYNTAX_UNIFIED__)
+		#if defined(__ARM_32BIT_STATE)
+			#define __32_BITS__
+		#elif defined(__ARM_64BIT_STATE)
+			#define __64_BITS__
+		#else
+			#error "Can't identify target ARM arch"
+		#endif
 	#else
-		#error "Unknown target"
+		#error "Unknown target architecture"
 	#endif
 #endif
 
