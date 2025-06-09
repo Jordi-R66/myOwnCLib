fe1e014bcd92df856f0cb4cb1f347fb07ab2fb27
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-06-09 09:30:55 +0200

    Réorganisation des sections de détection d'architecture et de système dans macros.h pour une meilleure lisibilité et maintenance.

--- targetSpecific/macros.h
+++ targetSpecific/macros.h
@@ -1,21 +1,35 @@
 #pragma once
 
 #ifndef OVERRIDE_DETECTION
-	#ifdef __x86_64__
-		#define __64_BITS__
-	#elif defined(__i386__) || defined(__i386)
-		#define __32_BITS__
-	#elif defined(__ARM_ASM_SYNTAX_UNIFIED__)
-		#if defined(__ARM_32BIT_STATE)
-			#define __32_BITS__
-		#elif defined(__ARM_64BIT_STATE)
+
+	#pragma region ArchDetection
+		#ifdef __x86_64__
 			#define __64_BITS__
+		#elif defined(__i386__) || defined(__i386) || defined(__i686__) || defined(__i686)
+			#define __32_BITS__
+		#elif defined(__ARM_ASM_SYNTAX_UNIFIED__)
+			#if defined(__ARM_32BIT_STATE)
+				#define __32_BITS__
+			#elif defined(__ARM_64BIT_STATE)
+				#define __64_BITS__
+			#else
+				#error "Can't identify target ARM arch"
+			#endif
 		#else
-			#error "Can't identify target ARM arch"
+			#error "Unknown target architecture"
 		#endif
-	#else
-		#error "Unknown target architecture"
-	#endif
+	#pragma endregion
+
+	#pragma region SysDetection
+		#ifdef __linux
+			#define LINUX
+		#elif defined(_WIN64) || defined(_WIN32)
+			#define WINDOWS
+		#else
+			#error "Unknown target OS"
+		#endif
+	#pragma endregion
+
 #endif
 
 #ifdef NULL
