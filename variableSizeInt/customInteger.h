b78797eae5b12b5b92b12b93665b603b78d4900d
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-06-10 19:42:11 +0200

    Ajout de la définition de CustomInteger et des fonctions associées dans customInteger.h

---
+++ variableSizeInt/customInteger.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../targetSpecific/types.h"
+#include "../bits/bits.h"
+#include "../bools/bool.h"
+
+#ifndef CUSTOM_INT
+#define CUSTOM_INT
+
+#define ADD_BIT_S(A, B, C_IN) (A ^ B ^ C_IN)
+#define ADD_BIT_C(A, B, C_IN) (A && B || (C_IN && (A ^ B)))
+
+#pragma pack(1)
+
+typedef struct customInt {
+	ptr value;
+	SizeT size;
+	SizeT capacity;
+	bool isNegative;
+} CustomInteger;
+
+typedef CustomInteger* custIntPtr;
+
+CustomInteger allocInteger(SizeT capacity);
+void reallocInteger(custIntPtr integer);
+void freeInteger(custIntPtr ineger);
+
+CustomInteger addInteger(CustomInteger a, CustomInteger b);
+CustomInteger subtractInteger(CustomInteger a, CustomInteger b);
+CustomInteger multiplyInteger(CustomInteger a, CustomInteger b);
+CustomInteger divideInteger(CustomInteger a, CustomInteger b);
+CustomInteger modInteger(CustomInteger a, CustomInteger b);
+
+CustomInteger powInteger(CustomInteger a, CustomInteger exp);
+
+#pragma pack()
+
+#endif
