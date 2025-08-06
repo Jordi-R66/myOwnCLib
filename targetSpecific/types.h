d9b1a3fe11282de09d8f6d6589fa5e9f8d883ce2
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-08-06 02:27:29 +0200

    Added MAX and MIN value for every numerical type, both signed and unsigned

--- targetSpecific/types.h
+++ targetSpecific/types.h
@@ -39,19 +39,68 @@ typedef signed short int16;
 	#endif
 #pragma endregion
 
+#define I8_SIZE sizeof(uint8)
+#define I16_SIZE sizeof(uint16)
+#define I32_SIZE sizeof(uint32)
+#define I64_SIZE sizeof(uint64)
+
+#define U8_MAX_VAL 0xFF
+#define U8_MIN_VAL 0x0
+#define I8_MAX_VAL  127
+#define I8_MIN_VAL -128
+
+#define U16_MAX_VAL 0xFFFF
+#define U16_MIN_VAL 0x0
+#define I16_MAX_VAL  32767
+#define I16_MIN_VAL -32768
+
+#define U32_MAX_VAL 0xFFFFFFFF
+#define U32_MIN_VAL 0x0
+#define I32_MAX_VAL  2147483647
+#define I32_MIN_VAL -2147483648
+
+#define U64_MAX_VAL 0xFFFFFFFFFFFFFFFF
+#define U64_MIN_VAL 0x0
+#define I64_MAX_VAL  9223372036854775807
+#define I64_MIN_VAL -9223372036854775808
+
 #pragma region SizeT Declaration
 	#if defined(LINUX) && defined(INTEL_ARCH_FAMILY) && defined(_32BITS)
 		typedef uint32 SizeT;
 		typedef int32 sSizeT;
+
+		#define SIZET_MAX_VAL U32_MAX_VAL
+		#define SIZET_MIN_VAL U32_MIN_VAL
+
+		#define SSIZET_MAX_VAL I32_MAX_VAL
+		#define SSIZET_MIN_VAL I32_MIN_VAL
 	#elif defined(LINUX) && defined(INTEL_ARCH_FAMILY) && defined(_64BITS)
 		typedef uint64 SizeT;
 		typedef int64 sSizeT;
+
+		#define SIZET_MAX_VAL U64_MAX_VAL
+		#define SIZET_MIN_VAL U64_MIN_VAL
+
+		#define SSIZET_MAX_VAL I64_MAX_VAL
+		#define SSIZET_MIN_VAL I64_MIN_VAL
 	#elif defined(WINDOWS) && defined(INTEL_ARCH_FAMILY) && defined(_32BITS)
 		typedef uint32 SizeT;
 		typedef int32 sSizeT;
+
+		#define SIZET_MAX_VAL U32_MAX_VAL
+		#define SIZET_MIN_VAL U32_MIN_VAL
+
+		#define SSIZET_MAX_VAL I32_MAX_VAL
+		#define SSIZET_MIN_VAL I32_MIN_VAL
 	#elif defined(WINDOWS) && defined(INTEL_ARCH_FAMILY) && defined(_64BITS)
 		typedef uint64 SizeT;
 		typedef int64 sSizeT;
+
+		#define SIZET_MAX_VAL U64_MAX_VAL
+		#define SIZET_MIN_VAL U64_MIN_VAL
+
+		#define SSIZET_MAX_VAL I64_MAX_VAL
+		#define SSIZET_MIN_VAL I64_MIN_VAL
 	#endif
 #pragma endregion
 
