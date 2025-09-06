d83aefcf877fb924b0287ba986d7f7b8b5ff7647
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-09-06 16:04:12 +0200

    Implementing a function to look up for unused bytes in the buffer to compress

--- compression/bpe.c
+++ compression/bpe.c
@@ -1,3 +1,37 @@
 #include "bpe.h"
 #include "../memory/memfuncs.h"
 #include "../bools/bool.h"
+
+MemBlock findUnusedBytes(MemBlock memBlock) {
+	MemBlock unused_bytes = {256, NULL};
+
+	uint8 bytes[256];
+	setMemory(&bytes, true, 256);
+
+	uint8* addr = (uint8*)memBlock.addr;
+
+	for (SizeT i = 0; i < memBlock.size; i++) {
+		uint8 byte = addr[i];
+
+		if (bytes[byte]) {
+			bytes[byte] = false;
+			unused_bytes.size--;
+
+			if (unused_bytes.size == 0) {
+				return (MemBlock){.size = 0, .addr = NULL};
+			}
+		}
+	}
+
+	unused_bytes.addr = calloc(unused_bytes.size, I8_SIZE);
+
+	uint16 j = 0;
+
+	for (uint16 i = 0; i < 256; i++) {
+		if (bytes[i]) {
+			((uint8*)unused_bytes.addr)[j++] = (uint8)i;
+		}
+	}
+
+	return unused_bytes;
+}
