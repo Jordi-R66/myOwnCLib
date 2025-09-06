fc5c62a9b2e018e8fb951c2c9174de2c17323cc6
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-09-06 17:11:39 +0200

    Moved Comparison typedefs into memtypes.h

--- memory/memtypes.h
+++ memory/memtypes.h
@@ -10,6 +10,13 @@ typedef struct {
 	ptr addr;
 } MemBlock;
 
+typedef enum Comparison {
+	LESS = 0, EQUALS = 1, GREATER = 2, UNDEF = 3
+} Comparison;
+
+// Création des fonctions de comparaison
+typedef Comparison (*ComparisonFunc)(ptr, ptr, SizeT);
+
 #define MEMBLOCK_SIZE sizeof(MemBlock)
 
 #pragma pack()
