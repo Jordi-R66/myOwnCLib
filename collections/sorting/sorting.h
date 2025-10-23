a7bbe1baab8cba1ffa95224b69a3ccf75cc2a483
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-10-23 09:44:17 +0200

    Started declaring the header for

---
+++ collections/sorting/sorting.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifndef SORTING_ALGORITHMS
+#include "../collection.h"
+
+typedef void (*SortingFunction)(ptr, SizeT, SizeT, SizeT, ComparisonFunc); // ptr: array, SizeT: element size
+#endif
