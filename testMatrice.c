d4135c72864e035efb293ff18aafe44593a62018
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-01-17 11:41:23 +0100

    Correction des inclusions et mise à jour des types de données dans testMatrice.c

--- testMatrice.c
+++ testMatrice.c
@@ -1,24 +1,26 @@
-#define OVERRIDE_MATRIX_VALUE_TYPE
-#define VALUE_TYPE int32
+//#define OVERRIDE_MATRIX_VALUE_TYPE
+//#define VALUE_TYPE int32
 
-#include "matrices/matrix.h"
+#include "maths/matrices/matrix.h"
 
 int32 main(void) {
 	Matrix M, C;
 
 	M.rows = 4;
 	M.cols = M.rows;
+	M.data = NULL;
+	M.memFreed = false;
 
 	allocMatrix(&M);
 
-	value_t vals[16] = {0, 1, 1, 1,
-						1, 2, 0, 0,
-						1, 0, 0, 1,
-						1, 0, 1, 0};
+	Value vals[16] = { 0.0, 1.0, 1.0, 1.0,
+						1.0, 2.0, 0.0, 0.0,
+						1.0, 0.0, 0.0, 1.0,
+						1.0, 0.0, 1.0, 0.0 };
 
 	setMatrix(&M, vals);
 
-	printMatrix(&M, S_DISCRETE);
+	//printMatrix(&M, S_DISCRETE);
 
 	matrixMultiplication(&M, &M, &C);
 
